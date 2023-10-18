1f60c4cf82785d32f2bb1b3d20b05d822da386b0
Piero Martell Falcon <105953521+Pierom689@users.noreply.github.com>
2023-10-18 13:43:55 -0500

    pequeña correccion

--- src/controller/Webada.js
+++
@@ -1,9 +0,0 @@
-const alredyintroduced = true;
-
-const introducemyself = (alredyintroduced) => {
-    if (!alredyintroduced) {
-        console.log("Hi, I'm Piero Martell, a web developer from Perú.");
-    } else {
-        console.log("Hello again, have a good day :]");
-    }
-}; 
